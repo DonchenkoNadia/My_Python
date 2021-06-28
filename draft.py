74133c223292cfac1cc789020f944361eda2ab98
Nadia <DonchenkoNadia@gmail.com>
2021-06-28 14:58:11 +0200

    Have tried valid parentheses task

--- draft.py
+++ draft.py
@@ -1,3 +1,74 @@
-image = [[1,1,0],[1,0,1],[0,0,0]]
-for i in range(0, len(image)):
-    print(image[i])
+#from collections import deque
+st = []
+s = "(((()))"
+br_open = ["(", "{", "["]
+br_close = [")", "}", "]"]
+
+l = ""
+def isValid(s):
+    for i in s:
+        if i in br_open:
+            st.append(i)
+            print(st)
+        if i in br_close:
+            l = st.pop()
+            print(f"st = {st}")
+            print(f"l = {l}")
+            print(f"i = {i}")
+            if l == "(" and i == ")":
+                continue
+            elif l == "[" and i == "]":
+                continue
+            elif l == "{" and i == "}":
+                continue
+            else:
+                return 0
+    if len(st) == 0:
+        return 1
+    elif len(st) > 0:
+        return 0
+
+if isValid(s):
+    print("Good")
+else:
+    print("No")
+
+'''
+([({})][]) - Yes
+[][) - No
+
+4*5+3*4
+->
+
+n
+n - пар скобок
+сколько существует вариантов правильных скобочных последовательностей?
+
+1/(n+1)*C(2n)_(n)
+
+3
+((()))
+(())()
+()()()
+()(())
+()()()
+
+5
+4 3
+
+[1, 1, 1 + 1, ...]
+
+n, k
+print(n^k)
+
+k - 5
+n^2 * n^2 * n
+k - 4
+n^2 * n^2
+
+1) Старые задачи
+2) обратная польская запись(нотация)
+3) Возведение в степень через рекурсию. Строго после предыдущего пункта. Посмотреть алогритм бинарного возведения в степень.
+4) def (n) :
+     return () -- определяет явлется ли n степенью двойки
+'''
--- one_more_draft.py
+++ one_more_draft.py
@@ -1,34 +1,19 @@
-'''
-Input: x = 1, y = 4
-Output: 2
-Explanation:
-1   (0 0 0 1)
-4   (0 1 0 0)
-       ↑   ↑
-The above arrows point to positions where the corresponding bits are different.
-'''
-def get_normalized_bit(value, bit_index):
-    return (value >> bit_index) & 1
+n = 10
+data = [-1]*n
+data[0] = 0
+data[1] = 1
 
-a = 3
-b = 1
+def fib(n):
+    if n == 1:
+        return 1
+    if n == 0:
+        return 0
+    else:
+        if data[n-2] == -1:
+            data[n-2] = fib(n-2)
+        if data[n-1] == -1:
+            data[n-1] = fib(n-1)
+        else:
+            return (data[n-2] + data[n-1])
 
-len = max(a.bit_length(), b.bit_length())
-#print(len)
-
-first_dif = 0
-first_bit = 0
-
-second_bit = 0
-
-for i in range(0, len):
-    if ((get_normalized_bit(a, i) != get_normalized_bit(b, i)) and first_dif == 0):
-        first_bit = i
-        first_dif = 1
-    if ((get_normalized_bit(a, i) != get_normalized_bit(b, i)) and first_dif == 1):
-        second_bit = i
-
-#print(f"first_bit = {first_bit}")
-#print(f"second_bit = {second_bit}")
-
-print(mod(second_bit-first_bit))
+print(fib(n))
