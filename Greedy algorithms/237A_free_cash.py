7a9a55d9af60d1e83e9a977d7abfd6ab7adc2b50
Nadia <DonchenkoNadia@gmail.com>
2021-09-06 11:21:17 +0200

    :)
    :)

--- Greedy algorithms/237A_free_cash.py
+++ Greedy algorithms/237A_free_cash.py
@@ -1,17 +1,17 @@
-#https://codeforces.com/problemset/problem/237/A?locale=en
 n = int(input())
-time = []
-for i in range(0, n):
-    time.append(list(map(int, input().split())))
-    
-ans = 1
 
-for i in range(1, n):
-    if (time[i][0] == time[i-1][0]):
-        diff = abs(time[i][1] - time[i-1][1])
-        if abs(diff) <= 1:
-            ans += 1
-    elif  (time[i][0] - time[i-1][0] == 1) and (time[i-1][1] == 59 and time[i][1] == 0):
-        ans += 1
+h0, m0 = map(int, input().split())
 
-print(ans)
+while n != 0:
+    h, m = map(int, input().split())
+    if h == h0 and m == m0:
+        c += 1
+        if c > cash:
+            cash = c
+    else:
+        c = 1
+    h0 = h
+    m0 = m
+    n -= 1
+
+print (cash)
